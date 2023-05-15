a2c40ef0ef695a05fd87b811018a769c8b6f6f9b
omides248 <omidesmaeeli4@gmail.com>
2023-05-15 22:13:39 +0330

    Set version 1.0.1

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 class Config:
-    version = "1.0.0"
+    version = "1.0.1"
     telegram_url = "https://t.me/IranDNSApp"
     github_url = "https://github.com/omides248/iran_dns"
     background_color = "white"
